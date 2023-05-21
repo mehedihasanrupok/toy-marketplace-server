f9247ba7a9d920d1cc62797d58dae3173657bef6
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-05-21 13:52:40 +0600

    Add Toy Delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -39,6 +39,24 @@ async function run() {
         const result = await toyCollection.find(query).toArray();
         res.send(result);
       })
+
+
+      app.delete('/addToy/:id', async(req,res)=>{
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    // app.get('/addToy', async(req,res) =>{
+    //     let query ={};
+    //     if(req.query?.email){
+    //       query = {email: req.query.email}
+    //     }
+    //     const result = await toyCollection.find().toArray();
+    //     res.send(result);
+    //   })
 
     app.post('/addToy', async(req,res) =>{
         const addToy = req.body;
