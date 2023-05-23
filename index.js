a93bb7a5b383c46d49866262e0cc254e9eaa604a
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-05-23 10:22:30 +0600

    VercelCors Update 2

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    client.connect();
 
     const toyCollection = client.db('toy-marketplace').collection('addToy');
 
